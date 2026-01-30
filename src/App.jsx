97fa126e62e2906ae2cd8c58da7ae43dc38027ac
DashginAsgarli <dashqinasgarli@gmail.com>
2026-01-31 03:07:33 +0400

    гзвфеу

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import HomePage from './component/Main/HomePage.jsx'
 import Serrvices from './component/Footer/Services.jsx'
 import Contact from './component/Footer/Contact.jsx'
 import Portfolio from './component/Main/Skills.jsx'
+import Education from './component/Main/Education.jsx'
 function App() {
 
   return (
     <>
     <HomePage />
+    <Education />
     <Portfolio />
     <Contact />
     <Serrvices />
